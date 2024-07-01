e25a9a26292bb86a9fac49982e028c3ee60052d4
renzo <renzo@rcz.com>
2024-06-30 19:06:14 -0500

    chore RCZ-001: First Commit

---
+++ src/main/java/com/example/rcz_git_example/RczGitExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.rcz_git_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RczGitExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RczGitExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rcz_git_example/RczGitExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rcz_git_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RczGitExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
