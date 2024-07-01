10af0259ad580f28981b7b0c407739ca8e2bb3eb
renzo <renzo@rcz.com>
2024-06-30 19:15:04 -0500

    test RCZ-002: Add test

--- src/test/java/com/example/rcz_git_example/RczGitExampleApplicationTests.java
+++ src/test/java/com/example/rcz_git_example/RczGitExampleApplicationTests.java
@@ -3,6 +3,8 @@ package com.example.rcz_git_example;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
+
 @SpringBootTest
 class RczGitExampleApplicationTests {
 
@@ -10,4 +12,6 @@ class RczGitExampleApplicationTests {
 	void contextLoads() {
 	}
 
+	private String additionalTest;
+	private BigDecimal totalPrice;
 }
